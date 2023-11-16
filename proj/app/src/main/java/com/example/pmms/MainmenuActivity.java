4d596895871d8b34942717f384a6e4127698bbca
MahnoorEjaz <ejazmahnoor8@gmail.com>
2023-11-17 01:21:41 +0500

    Home Page

--- proj/app/src/main/java/com/example/pmms/MainmenuActivity.java
+++ proj/app/src/main/java/com/example/pmms/MainmenuActivity.java
@@ -2,13 +2,89 @@ package com.example.pmms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainmenuActivity extends AppCompatActivity {
+        ImageButton prev,next;
+        ImageView pic;
+        int currentimg =0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
     }
+    public void prev(View v){
+
+//        text = findViewById(R.id.text);
+//
+        String idX = "pic" + currentimg;
+
+        int x = this.getResources().getIdentifier(idX, "id", getPackageName());
+
+        pic = findViewById(x);
+
+        pic.setAlpha(0f);
+
+
+
+        currentimg = (3 + currentimg - 1) % 3;
+
+        String idY = "pic" + currentimg;
+
+        int y = this.getResources().getIdentifier(idY, "id", getPackageName());
+
+        pic = findViewById(y);
+
+        pic.setAlpha(1f);
+//
+//
+//
+//        text.setText(names[currentImage]);
+
+
+
+    }
+    public void next(View v){
+        String idX = "pic" + currentimg;
+
+        int x = this.getResources().getIdentifier(idX, "id", getPackageName());
+
+        pic = findViewById(x);
+
+        pic.setAlpha(0f);
+
+
+
+        currentimg = (3 + currentimg + 1) % 3;
+
+        String idY = "pic" + currentimg;
+
+        int y = this.getResources().getIdentifier(idY, "id", getPackageName());
+
+        pic = findViewById(y);
+
+        pic.setAlpha(1f);
+    }
+    public void outdoorposter(View v)
+    {
+        Intent intent = new Intent(this, outdoorPosterActivity.class);
+        startActivity(intent);
+    }
+    public void poster(View v)
+    {
+        Intent intent = new Intent(this, posterActivity.class);
+        startActivity(intent);
+    }
+    public void Banners(View v)
+    {
+        Intent intent = new Intent(this, bannerActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ proj/app/src/main/java/com/example/pmms/posterActivity.java
@@ -0,0 +1,14 @@
+package com.example.pmms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class posterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_poster);
+    }
+}
